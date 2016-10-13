2a939b0d4098407ffb4930d1aac8c6bdfe925c90
lon.yang <634377375@qq.com>
2016-10-13 15:36:04 +0800

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  template: `
+    <h1>Hello from Angular App with Webpack</h1>
+  `
+})
+export class AppComponent {
+  constructor() {
+
+  }
+
+  ngAfterViewInit() {
+    
+  }
+}
